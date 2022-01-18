7ca3b87e9e8bb42a50cea454e40f71f70f808565
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2022-01-18 17:55:53 +0100

    Added project package structure

---
+++ src/main/java/com/javastriker69/blog/RexcoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.javastriker69.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RexcoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RexcoBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javastriker69/blog/RexcoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javastriker69.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RexcoBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
