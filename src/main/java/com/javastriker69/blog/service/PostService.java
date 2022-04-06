7ecea01bde32e53807b4173412d203a7ae76d795
Emeka Paul Ukwuoma <70348184+ChukwuEmeka1269@users.noreply.github.com>
2022-04-06 15:02:45 +0100

    Update PostService.java

--- src/main/java/com/javastriker69/blog/service/PostService.java
+++ src/main/java/com/javastriker69/blog/service/PostService.java
@@ -11,9 +11,4 @@ public interface PostService {
     PostDto updatePost(Long postId, PostDto newPostDto);
     void deletePosts();
     void deletePostById(Long postId);
-//    PostDto updatePostTitle(Long postId, String newPostDto);
-//    PostDto updatePostDescription(Long postId, String newDescription);
-//    PostDto updatePostContent(Long postId, String newDescription);
-
-
 }
